a918a6cb1fbba62ddd172a4d06bc5dfbab8ba3a1
mbouzi <mrbouzi@gmail.com>
2018-03-13 09:06:59 -0400

    header styling

--- src/components/GifCard.js
+++ src/components/GifCard.js
@@ -6,7 +6,7 @@ import styled from 'styled-components';
 const GifCard = (props) => {
 	const Image = styled.img`
 		height: 200px;
-		width: 300px;
+		width: 270px;
 	    margin: 10px;
 	    box-shadow: 0 0 0 1px rgba(212, 212, 213, 0.1), 0 4px 0 0 ${props.color}, 0 1px 3px 0 rgba(212, 212, 213, 0.1)
 	`
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Header, Segment} from 'semantic-ui-react'
+import { Header, Segment, Icon} from 'semantic-ui-react'
+import styled from 'styled-components';
 
+const Title = styled.h1`
+    color: #fff
+`
 
 class NavBar extends React.Component {
     render() {
         return (
-            <Segment>
-                <Header textAlign='center'>
-                    <Link to='/'><img src="../assets/images/giphy-logo.png" /></Link>
-                </Header>
-            </Segment>
+            <Header as='h2' icon textAlign='center'>
+                <Icon inverted name='find' circular />
+                <Header.Content>
+                    <Link to='/'><Title>Gif Searcher</Title></Link>
+                </Header.Content>
+            </Header>
         );
     }
 }
